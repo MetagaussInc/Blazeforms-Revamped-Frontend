921ea14409d4879f2bdcf30af744ba72480022d6
RSHUBH <rohit.suthar.developer@gmail.com>
2021-07-19 17:40:32 +0530

    Added search functionality.

--- src/app/ux/t-select/t-select.component.ts
+++ src/app/ux/t-select/t-select.component.ts
@@ -2,13 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {FlatTreeControl} from '@angular/cdk/tree';
 import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
 
-
-interface FoodNode {
-  name: string;
-  main?: boolean;
-  children?: FoodNode[];
-}
-
 const TREE_DATA: FoodNode[] = [
   {
     name: 'Root',
@@ -42,9 +35,18 @@ const TREE_DATA: FoodNode[] = [
 interface ExampleFlatNode {
   expandable: boolean;
   name: string;
-  level: number;
+  level: number; 
+  // fullFolderPath?: any[], 
+  fullFolderPath?: string[], 
 }
 
+interface FoodNode {
+  name: string;
+  main?: boolean;
+  children?: FoodNode[];
+  fullFolderPath?: string;
+  // fullFolderPath?: any[];
+}
 @Component({
   selector: 'app-t-select',
   templateUrl: './t-select.component.html',
@@ -52,24 +54,71 @@ interface ExampleFlatNode {
 })
 export class TSelectComponent implements OnInit { 
   selected = 'Root';
+  searchedString = '';
   private _transformer = (node: FoodNode, level: number) => {
     return {
       expandable: !!node.children && node.children.length > 0,
       name: node.name,
       level: level,
+      fullFolderPath: node.fullFolderPath
     };
   }
+  search($event: any) {
+    // console.log($event.target.value)
+    // this.dataSource.data = TREE_DATA
+  }
 
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
+  treeControl = new FlatTreeControl<any>(
       node => node.level, node => node.expandable);
 
   treeFlattener = new MatTreeFlattener(
-      this._transformer, node => node.level, node => node.expandable, node => node.children);
+      this._transformer, node => node.level, node => node.expandable, node => node.children );
 
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
+
+
+
+
+  
   constructor() {
+
+    const o =  TREE_DATA.forEach(element => {
+      // element.fullFolderPath = [];
+      // element.fullFolderPath.push(element.name);
+      element.fullFolderPath = '';
+      element.fullFolderPath += '/'+ element.name.toLowerCase();
+      const parentData = [];
+      parentData.push(element);
+      if (element.children) {
+        this.ExtractChild(parentData, element.children)
+        
+      }
+
+    })
+    console.log(TREE_DATA)
     this.dataSource.data = TREE_DATA;
+
+  }
+
+  ExtractChild(parentElements: any, childElements: any) {
+    childElements.forEach((child: any) => {
+      // child.fullFolderPath = [];
+      // child.fullFolderPath.push(child.name);
+      child.fullFolderPath = '';
+      child.fullFolderPath += '/' + child.name.toLowerCase();
+      parentElements.forEach((element: any) => {
+        element.fullFolderPath += '/' + child.name.toLowerCase();
+       
+      });
+      const parent = []
+      parent.push(...parentElements);
+      parent.push(child);
+      if (child.children) {
+        this.ExtractChild(parent, child.children)
+        
+      }
+    });
   }
   select(node: any): void {
     console.log(node)
