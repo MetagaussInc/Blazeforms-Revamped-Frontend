b13a87949df1d71fac8e360f453d720d6b1a07c7
RSHUBH <rohit.suthar.developer@gmail.com>
2021-09-07 08:11:03 +0530

    Copy existing functionality.

--- src/app/projects/forms/components/add-form-modal/add-form-modal.component.ts
+++ src/app/projects/forms/components/add-form-modal/add-form-modal.component.ts
@@ -1,5 +1,9 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import * as lodash from 'lodash';
+import { HttpService } from 'src/app/config/rest-config/http.service';
 
 @Component({
   selector: 'app-add-form-modal',
@@ -8,9 +12,15 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AddFormModalComponent implements OnInit, OnChanges {
 @Input() public formsList: any;
+@Input() public userId: any;
 @Output() openEditEvent: EventEmitter<any> = new EventEmitter()
  viewFormsList = false;
-  constructor(public activeModal: NgbActiveModal) {
+ viewFormDesign = false;
+ viewMainSection = true;
+ url = '';
+ selectedForm: any = null;
+ selectedFormDetail: any = null;
+  constructor(private router: Router,public activeModal: NgbActiveModal, private sanitizer: DomSanitizer, private http: HttpService) {
 
    }
 
@@ -26,9 +36,78 @@ export class AddFormModalComponent implements OnInit, OnChanges {
   }
 
   copy(form: any) {
+    this.selectedForm = form;
+    this.getForm();
 
   }
   viewForm(form: any) {
+    this.url = form.url;
+    this.selectedForm = form;
+    this.viewFormDesign = true;
+    this.viewFormsList = false
+  }
+
+  copyFromDesign() {
+    this.getForm();
+  }
+
+  FormUrl() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(window.location.href?.split('#')?.[0] + `#/${this.url?.split('#')?.[1]?.replace('BlazeForms', 'blazeforms')}`);
+  }
+
+  getForm() {
+    const payload = {
+      FormEntriesId: null, // to do
+      Id: this.selectedForm.id,
+      Name: null, // to do
+      WorkSpaceName: null, // to do
+      userID: this.userId
+    }
+    this.http.call('GetFormDesign', 'POST', payload).subscribe(res => {
+      this.selectedFormDetail = res;
+      this.copyAttributes(res);
+  
+    })
 
   }
+
+  copyAttributes(formDetail: any) {
+    const payload = {
+      CreatedBy: formDetail.createdBy,
+DependenciesJSON: formDetail.dependenciesJSON,
+Description: formDetail.description,
+FormNewJSON: formDetail.formNewJSON,
+FormSettings: formDetail.formSettings,
+FormStyleJson: formDetail.formStyleJson,
+FormType: formDetail.formType,
+Id: formDetail.id,
+Name: this.getNewFormName(formDetail.name + 'copy '),
+SubmissionButtonName: formDetail.submissionButtonName,
+SubmissionDependenciesJSON: formDetail.submissionDependenciesJSON,
+SubmissionSettings: formDetail.submissionSettings,
+WorkSpaceId: formDetail.workSpaceId,
+formLabels: formDetail.formLabels,
+modifiedBy: formDetail.modifiedBy,
+miscellaneousJSON: formDetail.miscellaneousJSON
+    }
+
+    this.http.call('CopyFormAttributes', 'POST', payload).subscribe(res => {
+      this.router.navigate(['/form-builder'], {queryParams: {ID: res.id, seletedTab: 1}})
+      this.activeModal.close('close');
+    })
+  }
+
+  getRandomIntInclusive(min: any, max: any) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; // max & min both included 
+  }
+
+  getNewFormName(formName: string) {
+    const randomNumber = this.getRandomIntInclusive(0, 10);
+    return formName + randomNumber;
+  }
+
 }
+
+
